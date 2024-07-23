62de3e4eb46814ecf40f313252a5ab0255771a80
1ly4s0 <67460389+1ly4s0@users.noreply.github.com>
2024-07-23 22:38:03 +0200

    Update build

---
+++ build.js
@@ -0,0 +1,44 @@
+const builder = require('electron-builder');
+const obfuscator = require('javascript-obfuscator');
+const fs = require('fs-extra');
+const path = require('path');
+const { productname } = require('./package.json');
+builder.build({
+    config: {
+        publish: [
+            {
+                provider: "github",
+                owner: "1ly4s0",
+                repo: "flashexplorer",
+                releaseType: "release"
+            }
+        ],
+        generateUpdatesFilesForAllChannels: true,
+        appId: productname,
+        productName: productname,
+        artifactName: '${productName}-${os}-${arch}.${ext}',
+        files: ["src/**/*", "package.json", "LICENSE.md"],
+        directories: { "output": "dist" },
+        compression: 'maximum',
+        asar: true,
+        win: {
+            icon: "./src/assets/images/icons/icon.ico",
+            target: [{
+                target: "nsis",
+                arch: ["x64", "ia32"]
+            }]
+        },
+        nsis: {
+            oneClick: false,
+            allowToChangeInstallationDirectory: true,
+            createDesktopShortcut: true,
+            runAfterFinish: true,
+            multiLanguageInstaller: true,
+            license: "./LICENSE.md",
+        }
+    }
+}).then(() => {
+    console.log('✅ El build se ha realizado correctamente.')
+}).catch(err => {
+    console.error('Error al realizar el build', err)
+})
